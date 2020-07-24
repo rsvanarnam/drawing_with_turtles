9b3eb5d79addaac138a4e814957c4ba48243a8aa
rsvanarnam <36892758+rsvanarnam@users.noreply.github.com>
2020-07-24 13:40:51 -0600

    Update Drawing-With-Turtles.py
    Refactored buttons, and arranged them in a significantly better looking grid. I need to figure out how to further refactor them, but it is an improvement.

--- Drawing-With-Turtles.py
+++ Drawing-With-Turtles.py
@@ -1,10 +1,21 @@
-import time
+"""
+To do:
+1. Figure out how to change color upon each new line (rainbow).
+
+2. Fix several circular and spiral patterns not making the desired pattern.
+
+3. Either figure out how to add and remove turtles, or find something else 
+of interest to fill those buttons.
+
+4. Refactor buttons. Perhaps a for loop with a list for each section?
+"""
+
 import tkinter as tk
 import turtle
 
 # Initializing Tkinter.
 root = tk.Tk()
-canvas = tk.Canvas(master = root, width = 1000, height = 700)
+canvas = tk.Canvas(master = root, width = 900, height = 600)
 canvas.grid(row=0, columnspan=40)
 
 # Initialize Turtle module
@@ -304,150 +315,90 @@ class Pen():
     def down():
         turtle.pendown()
 
-# Row 2 Draw Forward, Colors.
-button = tk.Button(root, text = '↑', command = Movement.forward)
-button.grid(column = 2, row = 2)
-button = tk.Label(root, text = 'Color')
-button.grid(column = 5, row = 2)
-button = tk.Button(root, text = 'Black', command = Color.black)
-button.grid(column = 6, row = 2)
-button = tk.Button(root, text = 'Blue', command = Color.blue)
-button.grid(column = 7, row = 2)
-button = tk.Button(root, text = 'Green', command = Color.green)
-button.grid(column = 8, row = 2)
-button = tk.Button(root, text = 'Orange', command = Color.orange)
-button.grid(column = 9, row = 2)
-button = tk.Button(root, text = 'Pink', command = Color.pink)
-button.grid(column = 10, row = 2)
-button = tk.Button(root, text = 'Red', command = Color.red)
-button.grid(column = 11, row = 2)
-button = tk.Button(root, text = 'Violet', command = Color.violet)
-button.grid(column = 12, row = 2)
-button = tk.Button(root, text = 'Yellow', command = Color.yellow)
-button.grid(column = 13, row = 2)
-button = tk.Button(root, text = 'Rainbow', command = Color.rainbow)
-button.grid(column = 14, row = 2)
+class Button:
+    def __init__(self, setText, setCommand, setRow, setColumn):
+        self.setText = setText
+        self.setCommand = setCommand
+        self.setRow = setRow
+        self.setColumn = setColumn
 
-# Row 3. Rotate Left and Right, Speed Settings, Stamp, Clear, Reset.
-button = tk.Button(root, text = '←', command = Movement.left)
-button.grid(column = 1, row = 3)
-button = tk.Button(root, text = '→', command = Movement.right)
-button.grid(column = 3, row = 3)
-button = tk.Label(root, text = 'Control')
-button.grid(column = 5, row = 3)
-button = tk.Button(root, text = 'Slowest', command = Speed.slowest)
-button.grid(column = 6, row = 3)
-button = tk.Button(root, text = 'Slow', command = Speed.slow)
-button.grid(column = 7, row = 3)
-button = tk.Button(root, text = 'Regular', command = Speed.regular)
-button.grid(column = 8, row = 3)
-button = tk.Button(root, text = 'Fast', command = Speed.fast)
-button.grid(column = 9, row = 3)
-button = tk.Button(root, text = 'Fastest', command = Speed.fastest)
-button.grid(column = 10, row = 3)
-button = tk.Button(root, text = 'Filler')
-button.grid(column = 11, row = 3)
-button = tk.Button(root, text = 'Stamp', command = Movement.stamp)
-button.grid(column = 12, row = 3)
-button = tk.Button(root, text = 'Clear', command = Movement.clear)
-button.grid(column = 13, row = 3)
-button = tk.Button(root, text = 'Reset', command = Movement.reset)
-button.grid(column = 14, row = 3)
+    def smallButton(setText, setCommand, setRow, setColumn):
+        button = tk.Button(root, text = setText, command = setCommand, height = 2, width = 4)
+        button.grid(row = setRow, column = setColumn)
 
-# Row 4. Draw Backward and Shapes
-button = tk.Button(root, text = '↓', command = Movement.back)
-button.grid(column = 2, row = 4)
-button = tk.Label(root, text = 'Shape')
-button.grid(column = 5, row = 4)
-button = tk.Button(root, text = 'Circle', command = Shape.circle)
-button.grid(column = 6, row = 4)
-button = tk.Button(root, text = 'Crescent', command = Shape.crescent)
-button.grid(column = 7, row = 4)
-button = tk.Button(root, text = 'Heart', command = Shape.heart)
-button.grid(column = 8, row = 4)
-button = tk.Button(root, text = 'Hexagon', command = Shape.hexagon)
-button.grid(column = 9, row = 4)
-button = tk.Button(root, text = 'Oval', command = Shape.oval)
-button.grid(column = 10, row = 4)
-button = tk.Button(root, text = 'Pentagon', command = Shape.pentagon)
-button.grid(column = 11, row = 4)
-button = tk.Button(root, text = 'Square', command = Shape.square)
-button.grid(column = 12, row = 4)
-button = tk.Button(root, text = 'Star', command = Shape.star)
-button.grid(column = 13, row = 4)
-button = tk.Button(root, text = 'Triangle', command = Shape.triangle)
-button.grid(column = 14, row = 4)
+    def mediumButton(setText, setCommand, setRow, setColumn):
+        button = tk.Button(root, text = setText, command = setCommand, height = 2, width = 10)
+        button.grid(row = setRow, column = setColumn)
 
-# Row 5. Circular Patterns.
-button = tk.Label(root, text = 'Circular Patterns')
-button.grid(column = 5, row = 5)
-button = tk.Button(root, text = 'Circle', command = Circular.circle)
-button.grid(column = 6, row = 5)
-button = tk.Button(root, text = 'Crescent', command = Circular.crescent)
-button.grid(column = 7, row = 5)
-button = tk.Button(root, text = 'Heart', command = Circular.heart)
-button.grid(column = 8, row = 5)
-button = tk.Button(root, text = 'Hexagon', command = Circular.hexagon)
-button.grid(column = 9, row = 5)
-button = tk.Button(root, text = 'Oval', command = Circular.oval)
-button.grid(column = 10, row = 5)
-button = tk.Button(root, text = 'Pentagon', command = Circular.pentagon)
-button.grid(column = 11, row = 5)
-button = tk.Button(root, text = 'Square', command = Circular.square)
-button.grid(column = 12, row = 5)
-button = tk.Button(root, text = 'Star', command = Circular.star)
-button.grid(column = 13, row = 5)
-button = tk.Button(root, text = 'Triangle', command = Circular.triangle)
-button.grid(column = 14, row = 5)
+# Directional Buttons
+Button.smallButton('↑', Movement.forward, 3, 2)
+Button.smallButton('←', Movement.left, 4, 1)
+Button.smallButton('→', Movement.right, 4, 3)
+Button.smallButton('↓', Movement.back, 5, 2)
 
-# Row 6. Spiral Patterns.
-button = tk.Label(root, text = 'Spiral Patterns')
-button.grid(column = 5, row = 6)
-button = tk.Button(root, text = 'Circle', command = Spiral.circle)
-button.grid(column = 6, row = 6)
-button = tk.Button(root, text = 'Crescent', command = Spiral.crescent)
-button.grid(column = 7, row = 6)
-button = tk.Button(root, text = 'Heart', command = Spiral.heart)
-button.grid(column = 8, row = 6)
-button = tk.Button(root, text = 'Hexagon', command = Spiral.hexagon)
-button.grid(column = 9, row = 6)
-button = tk.Button(root, text = 'Oval', command = Spiral.oval)
-button.grid(column = 10, row = 6)
-button = tk.Button(root, text = 'Pentagon', command = Spiral.pentagon)
-button.grid(column = 11, row = 6)
-button = tk.Button(root, text = 'Square', command = Spiral.square)
-button.grid(column = 12, row = 6)
-button = tk.Button(root, text = 'Star', command = Spiral.star)
-button.grid(column = 13, row = 6)
-button = tk.Button(root, text = 'Triangle', command = Spiral.triangle)
-button.grid(column = 14, row = 6)
+# Color Change Buttons
+#Label.label('Color', 2, 5)
+Button.mediumButton('Black', Color.black, 2, 6)
+Button.mediumButton('Blue', Color.blue, 2, 7)
+Button.mediumButton('Green', Color.green, 2, 8)
+Button.mediumButton('Orange', Color.orange, 2, 9)
+Button.mediumButton('Pink', Color.pink, 2, 10)
+Button.mediumButton('Red', Color.red, 2, 11)
+Button.mediumButton('Violet', Color.violet, 2, 12)
+Button.mediumButton('Yellow', Color.yellow, 2, 13)
+Button.mediumButton('Rainbow', Color.rainbow, 2, 14)
 
-# Row 7. Pen up, Pen down.
-button = tk.Button(root, text = 'Pen Up', command = Pen.up)
-button.grid(column = 1, row = 7)
-button = tk.Button(root, text = 'Pen Down', command = Pen.down)
-button.grid(column = 3, row = 7)
+# Speed, Stamp, Clear, Reset Buttons
+#Label.label('Control', 3, 5)
+Button.mediumButton('Slowest\nSpeed', Speed.slowest, 3, 6)
+Button.mediumButton('Slow\nSpeed', Speed.slow, 3, 7)
+Button.mediumButton('Normal\nSpeed', Speed.regular, 3, 8)
+Button.mediumButton('Fast\nSpeed', Speed.fast, 3, 9)
+Button.mediumButton('Fastest\nSpeed', Speed.fastest, 3, 10)
+Button.mediumButton('Pen\nUp', Pen.up, 3, 11)
+Button.mediumButton('Pen\nDown', Pen.down, 3, 12)
+Button.mediumButton('Add\nTurtle', Color.rainbow, 3, 13)
+Button.mediumButton('Remove\nTurtle', Color.rainbow, 3, 14)
 
-# Row 8. Add and Remove Turtles, Turtle Race, Reset, Quit.
-button = tk.Button(root, text = 'Add Turtle')
-button.grid(column = 1, row = 8)
-button = tk.Button(root, text = 'Remove Turtle')
-button.grid(column = 3, row = 8)
-button = tk.Button(root, text = 'Turtle Race')
-button.grid(column = 12, row = 8)
-button = tk.Button(root, text = 'Reset', command = Movement.reset)
-button.grid(column = 13, row = 8)
-button = tk.Button(root, text = 'Quit', command = root.destroy)
-button.grid(column = 14, row = 8)
+# Shape Buttons
+#Label.label('Shape', 4, 5)
+Button.mediumButton('Circle', Shape.circle, 4, 6)
+Button.mediumButton('Crescent', Shape.crescent, 4, 7)
+Button.mediumButton('Heart', Shape.heart, 4, 8)
+Button.mediumButton('Hexagon', Shape.hexagon, 4, 9)
+Button.mediumButton('Oval', Shape.oval, 4, 10)
+Button.mediumButton('Pentagon', Shape.pentagon, 4, 11)
+Button.mediumButton('Square', Shape.square, 4, 12)
+Button.mediumButton('Star', Shape.star, 4, 13)
+Button.mediumButton('Triangle', Shape.triangle, 4, 14)
 
-root.grid_columnconfigure(4, minsize=100)
+# Circular Pattern Buttons
+#Label.label('Circle Pattern', 5, 5)
+Button.mediumButton('Circular\nCircles', Circular.circle, 5, 6)
+Button.mediumButton('Circular\nCrescents', Circular.crescent, 5, 7)
+Button.mediumButton('Circular\nHearts', Circular.heart, 5, 8)
+Button.mediumButton('Circular\nHexagons', Circular.hexagon, 5, 9)
+Button.mediumButton('Circular\nOvals', Circular.oval, 5, 10)
+Button.mediumButton('Circular\nPentagons', Circular.pentagon, 5, 11)
+Button.mediumButton('Circular\nSquares', Circular.square, 5, 12)
+Button.mediumButton('Circular\nStars', Circular.star, 5, 13)
+Button.mediumButton('Circular\nTriangles', Circular.triangle, 5, 14)
 
-col_count, row_count = root.grid_size()
+# Spiral Pattern Buttons
+#Label.label('Spiral Pattern', 6, 5)
+Button.mediumButton('Spiral\nCircles', Spiral.circle, 6, 6)
+Button.mediumButton('Spiral\nCrescents', Spiral.crescent, 6, 7)
+Button.mediumButton('Spiral\nHearts', Spiral.heart, 6, 8)
+Button.mediumButton('Spiral\nHexagons', Spiral.hexagon, 6, 9)
+Button.mediumButton('Spiral\nOvals', Spiral.oval, 6, 10)
+Button.mediumButton('Spiral\nPentagons', Spiral.pentagon, 6, 11)
+Button.mediumButton('Spiral\nSquares', Spiral.square, 6, 12)
+Button.mediumButton('Spiral\nStars', Spiral.star, 6, 13)
+Button.mediumButton('Spiral\nTriangles', Spiral.triangle, 6, 14)
 
-for col in range(14):
-    root.grid_columnconfigure(col, minsize=35)
-
-for row in range(8):
-    root.grid_rowconfigure(row, minsize=35)
+# Clear, Reset, Quit Buttons
+Button.mediumButton('Clear', Movement.clear, 8, 12)
+Button.mediumButton('Reset', Movement.reset, 8, 13)
+Button.mediumButton('Quit', root.destroy, 8, 14)
 
 root.mainloop()
